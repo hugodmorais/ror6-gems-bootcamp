ed6efb9ec2a5220e4bc7ffccdf46f767881769f7
hugodmorais <hugodanielmorais@gmail.com>
2021-05-29 22:07:48 +0000

    Add friendly_id

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,12 +1,21 @@
 class Course < ApplicationRecord
   has_rich_text :description
-
+  
   validates :title, presence: true
   validates :description, presence: true, length: { minimum: 5 }
-
+  
   belongs_to :user
+  
+  extend FriendlyId
+  friendly_id :generate_slug, use: :slugged
+
+  def generate_slug
+    require 'securerandom'
+    
+    @random_slug ||= persisted? ? friendly_id : SecureRandom.hex(4)
+  end
 
   def to_s
-    description
+    slug
   end
 end
