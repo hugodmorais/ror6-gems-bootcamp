40811c8b61adcc43ab8d7a3a0fbbc0ab4ff41832
hugodmorais <hugodanielmorais@gmail.com>
2021-06-01 23:12:49 +0000

    Add new fields to courses

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@ User.create!(email: 'admin@admin.com', password: '123456', password_confirmation
   Course.create!([{
     title: Faker::Educator.course_name,
     description: Faker::TvShows::GameOfThrones.quote,
-    user_id: User.first.id
+    user_id: User.first.id,
+    short_description: Faker::TvShows::GameOfThrones.quote,
+    language: Faker::ProgrammingLanguage.name,
+    level: 'Beginner',
+    price: Faker::Number.between(from: 1000, to: 2000)
   }])
 end
