992756109fd445ec67ca53602479d9177f2df2d5
hugodmorais <hugodanielmorais@gmail.com>
2021-06-04 08:38:43 +0000

    Add gem public_activity, activity page, titles and footer partial

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,7 @@
 class Course < ApplicationRecord
+  include PublicActivity::Model
+  tracked owner: Proc.new { |controller, model| controller.current_user }
+
   has_rich_text :description
   
   validates :title, :short_description, :language, :price, :level, presence: true
