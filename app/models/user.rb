9109d0730b3c53de811e911b34da4a8ec1d71d38
hugodmorais <hugodanielmorais@gmail.com>
2021-06-05 19:02:58 +0000

    Add roles to User with rolify gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
-
+  
+  rolify
   has_many :courses
+  after_create :assign_default_role
 
   def to_s
     email
@@ -13,4 +15,15 @@ class User < ApplicationRecord
   def username
     self.email.split(/@/).first
   end
+
+  def assign_default_role
+    if User.count == 1
+      add_role(:admin) if roles.blank?
+      add_role(:teacher)
+      add_role(:student)
+    else
+      add_role(:teacher)
+      add_role(:student)
+    end
+  end 
 end
