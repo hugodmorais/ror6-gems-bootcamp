9b07cc60a975e28058e18948122d11173c73b27a
hugodmorais <hugodanielmorais@gmail.com>
2021-06-18 08:17:29 +0000

    minor fix

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,6 @@
 class Course < ApplicationRecord
   include PublicActivity::Model
-  tracked owner: Proc.new { |controller, model| controller.current_user }
+  tracked owner: Proc.new { |controller, model| controller&.current_user }
 
   has_rich_text :description
   
