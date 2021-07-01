ca24872d5caad87c4c887dab77cd6f6a5dd8394f
hugodmorais <hugodanielmorais@gmail.com>
2021-07-01 22:27:17 +0000

    Clean routes charts, add chart money makers

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,15 @@ Rails.application.routes.draw do
     resources :lessons
   end
   resources :users, only: [:index, :edit, :show, :update]
+
+  namespace :charts do
+    get 'users_per_day'
+    get 'enrollments_per_day'
+    get 'course_popularity'
+    get 'money_makers'
+  end
   get 'home/index'
   get 'activity', to: 'home#activity'
   get 'analytics', to: 'home#analytics'
-  get 'charts/users_per_day', to: 'charts#users_per_day'
-  get 'charts/enrollments_per_day', to: 'charts#enrollments_per_day'
-  get 'charts/course_popularity', to: 'charts#course_popularity'
   root 'home#index'
 end
