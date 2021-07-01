94108e41a05a55544cfac5abf0de8eaea38649c2
hugodmorais <hugodanielmorais@gmail.com>
2021-07-01 22:20:50 +0000

    Send data to charts by json

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,8 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'activity', to: 'home#activity'
   get 'analytics', to: 'home#analytics'
+  get 'charts/users_per_day', to: 'charts#users_per_day'
+  get 'charts/enrollments_per_day', to: 'charts#enrollments_per_day'
+  get 'charts/course_popularity', to: 'charts#course_popularity'
   root 'home#index'
 end
