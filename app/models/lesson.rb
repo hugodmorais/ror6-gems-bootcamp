17b73acebc7db439434f598a1d806471ffb8708b
hugodmorais <hugodanielmorais@gmail.com>
2021-07-18 16:13:20 +0000

    Update show to video lesson

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,10 +1,16 @@
 class Lesson < ApplicationRecord
+  has_one_attached :video
+  has_one_attached :video_thumbnail
+
   belongs_to :course, counter_cache: true
   has_many :user_lessons, dependent: :destroy
 
   validates :title, :content, :course, presence: true
-  validates :title, uniqueness: true, lenght: { maximum: 70 }
-
+  validates :title, length: { maximum: 70 }
+  validates_uniqueness_of :title, scope: :course_id
+  validates :video, content_type: ['video/mp4'], size: { less_than: 50.megabytes, message: 'size should be under 50 megabytes' }
+  validates :video_thumbnail, content_type: ['image/png', 'image/jpg', 'image/jpeg'], size: { less_than: 1000.kilobytes, message: 'size should be under 1000 kilobytes' }
+  # validates :video_thumbnail, presence: true, if: :video_present?
   has_rich_text :content
 
   extend FriendlyId
@@ -20,6 +26,10 @@ class Lesson < ApplicationRecord
   def to_s
     title
   end
+
+  # def video_present?
+  #   self.video.present?
+  # end
 
   def prev
     course.lessons.where('row_order < ?', row_order).order(:row_order).last
