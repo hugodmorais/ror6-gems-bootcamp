532343129ab1aa74d8cf14b3c52ca7374ee9b948
hugodmorais <hugodanielmorais@gmail.com>
2021-08-05 21:06:40 +0000

    Update fields wicked partials

--- app/controllers/courses/course_wizard_controller.rb
+++ app/controllers/courses/course_wizard_controller.rb
@@ -1,17 +1,32 @@
 class Courses::CourseWizardController < ApplicationController
   include Wicked::Wizard
-  before_action :set_progress, only: [:show]
+  before_action :set_progress, only: [:show, :update]
+  before_action :set_course, only: [:show, :update, :finish_wizard_path]
 
   steps :basic_info, :details
 
   def show
-    @course = Course.friendly.find(params[:course_id])
     # @user = current_user
-    # case step
+    case step
+    when :basic_info
+    when :details
+      @tags = Tag.all
+    end
     # when :find_friends
     #   @friends = @user.find_friends
     # end
     render_wizard
+  end
+
+  def update
+    case step
+    when :basic_info
+      @course.update(course_params)
+    when :details
+      @tags = Tag.all
+      @course.update(course_params)
+    end
+    render_wizard @course
   end
 
   def finish_wizard_path
@@ -26,5 +41,13 @@ class Courses::CourseWizardController < ApplicationController
     else
       @progress = 0
     end
+  end
+
+  def set_course
+    @course = Course.friendly.find(params[:course_id])
+  end
+
+  def course_params
+    params.require(:course).permit(:title, :description, :short_description, :language, :price, :level, :published, :avatar, tag_ids: [])
   end
 end
